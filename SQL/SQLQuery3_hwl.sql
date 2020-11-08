74e8eeede2f654d3698dd35fc4f9d445ec66c383
PotatoB <1363964141@qq.com>
2020-11-08 10:35:33 +0000

    sql实验3

--- SQL/SQLQuery3_hwl.sql
+++ SQL/SQLQuery3_hwl.sql
@@ -1,109 +1,21 @@
---ÊµÑé2µÄ±í
-create database BookDB;
---·ÖÀà±í
-create table BookClass (
-	classNo char(3) constraint PK_BookClass_classNO primary key,
-	className char(20) not null constraint UK_BookClass_className unique
-);
---Í¼Êé±í
-create table Book (
-	bookNO char(10) constraint PK_Book_bookNO primary key,
-	classNO char(3) constraint FK_Book_classNO foreign key references BookClass(classNO),
-	bookName char(40) not null,
-	authorName char(8),
-	publishingName char(20),
-	publishingNO char(17) constraint UK_Book_publishingNO unique,
-	price decimal(7,2),
-	publishingDate date,
-	shopDate date,
-	shopNum int
-);
---¶ÁÕß±í
-create table Reader (
-	readerNO char(8) constraint PK_Reader_readerNO primary key,
-	readerName char(8),
-	sex char(2) constraint CK_Reader_sex check(sex='F' or sex='M'),
-	identitycard char(18) not null constraint UK_Reader_identitycard unique,
-	workUnit char(50),
-	borrowCount smallint not null
-);
---½èÔÄ±í
-create table Borrow (
-	readerNO char(8),
-	bookNO char(10),
-	borrowDate date,
-	shouldDate date,
-	returnDate date,
-	constraint PK_Borrow_readerNO_bookNO_borrowDate primary key(readerNO,bookNO,borrowDate),
-	constraint FK_Borrow_readerNO foreign key(readerNO) references Reader(readerNO),
-	constraint FK_Borrow_bookNO foreign key(bookNO) references Book(bookNO),
-);
---·ÖÀà±í²åÈë
-insert into BookClass values ('001','¾­¼ÃÀà');
-insert into BookClass values ('002','ÍâÓïÀà');
-insert into BookClass values ('003','¼ÆËã»úÀà');
---Í¼Êé±í²åÈë
-insert into Book values ('B200101001','001','ÕşÖÎ¾­¼ÃÑ§','ËÎÌÎ','ÖĞ¹úÈËÃñ´óÑ§³ö°æÉç','P1991010100010001',31.8,'19910101','19950111',100);
-insert into Book values ('B200201001','002','´óÑ§Ó¢Óï','Ö£Ê÷ÌÄ','ÍâÓï½ÌÑ§ÓëÑĞ¾¿³ö°æÉç','P1992010100010001',35.2,'19920101','19960111',130);
-insert into Book values ('B200301001','003','Êı¾İ¿âÏµÍ³Ô­Àí','Îâ¾©»Û','Çå»ª´óÑ§³ö°æÉç','P1993010100010001',58.2,'19930101','19950111',160);
-insert into Book values ('B200101002','001','Î¢¹Û¾­¼ÃÑ§','ÕÅÈï','¸ßµÈ½ÌÓı³ö°æÉç','P1991010100010002',41.8,'19910102','19950112','110');
-insert into Book values ('B200101003','001','ºê¹Û¾­¼ÃÑ§','Ô¬Ã÷Ê¥','ÖĞ¹ú²Æ¾­¾­¼Ã³ö°æÉç','P1991010100010003',51.8,'19910103','19950113',120);
-insert into Book values ('B200201002','002','ÉÌÎñÓ¢Óï','ÂíÉıìÇ','ÉÏº£ÍâÓïÑ§Ôº³ö°æÉç','P1992010100010002',45.2,'19920102','19960112',140);
-insert into Book values ('B200201003','002','ÉÌÎñÓ¢Óï2','½­Óî¼Ñ','Î÷°²½»Í¨´óÑ§³ö°æÉç','P1992010100010003',55.2,'19920103','19960113',150);
-insert into Book values ('B200301002','003','×éÍø¼¼Êõ','ÍòÕ÷','Õã½­´óÑ§³ö°æÉç','P1993010100010002',38.2,'19930102','19950112',170);
-insert into Book values ('B200301003','003','ÈË¹¤ÖÇÄÜ','·ÑÏèÁÖ','µç×Ó¹¤Òµ³ö°æÉç','P1993010100010003',43.2,'19930103','19950113',180);
-insert into Book values ('B200301004','003','Ëã·¨Éè¼ÆÓë·ÖÎö','³Â»ÛÄÏ','¿ÆÑ§³ö°æÉç','P1993010100010004',58.2,'19930104','19950114',190);
---¶ÁÕß±í²åÈë
-insert into Reader values ('R2005001','ÕÅĞ¡¾ê','F','412723199001014000','Í³Ò»¹É·İÓĞÏŞ¹«Ë¾',0);
-insert into Reader values ('R2006001','Áõ·ï','F','412723199003014000','ÁªºÏ¹É·İÓĞÏŞ¹«Ë¾',0);
-insert into Reader values ('R2007001','¸ß´úÅô','M','412723199005014000','ºé¶¼¹É·İÓĞÏŞ¹«Ë¾',0);
-insert into Reader values ('R2008001','³Â»Ô','M','412723199111014000','ÄÏ²ıÊĞµçÄÔÑĞÖÆ¹«Ë¾',0);
-insert into Reader values ('R2009001','Àîºç±ù','F','412723199208014000','¸»Ê¿¿µ¿Æ¼¼¼¯ÍÅ',0);
-insert into Reader values ('R2005002','ÕÅÂ¶','F','412723199002014000','ĞËÂ¡¹É·İÓĞÏŞ¹«Ë¾',0);
-insert into Reader values ('R2006002','Ó÷×ÔÇ¿','M','412723199004014000','ÍòÊÂ´ï¹É·İÓĞÏŞ¹«Ë¾',0);
-insert into Reader values ('R2007002','ÕÅÏşÃ·','F','412723199112014000','ÊÀ½ç¼¼Êõ¿ª·¢¹«Ë¾',0);
-insert into Reader values ('R2008002','ÕÅÁ¼','M','412723199110014000','ÉÏº£ÉúÎïÑĞ¾¿ÊÒ',0);
-insert into Reader values ('R2009002','º«¸£Æ½','M','412723199209014000','ºÏÉúÔªÓĞÏŞ¹«Ë¾',0);
---½èÔÄ±í²åÈë
-insert into Borrow values ('R2005001','B200101001','20110901','20111001',null);
-insert into Borrow values ('R2006001','B200101001','20110907','20111007','20111005');
-insert into Borrow values ('R2007001','B200201001','20110913','20111013','20111010');
-insert into Borrow values ('R2008001','B200301001','20110915','20111015','20111013');
-insert into Borrow values ('R2009001','B200301001','20110918','20111018',null);
-insert into Borrow values ('R2005001','B200101002','20110902','20111002','20111001');
-insert into Borrow values ('R2005001','B200101003','20110903','20111003',null);
-insert into Borrow values ('R2005002','B200101001','20110904','20111004','20111003');
-insert into Borrow values ('R2005002','B200101002','20110905','20111005',null);
-insert into Borrow values ('R2005002','B200101003','20110906','20111006','20111008');
-insert into Borrow values ('R2006001','B200101002','20110908','20111008','20111006');
-insert into Borrow values ('R2006001','B200101003','20110909','20111009','20111012');
-insert into Borrow values ('R2009002','B200201001','20110910','20111010','20111008');
-insert into Borrow values ('R2006002','B200201002','20110911','20111011','20111008');
-insert into Borrow values ('R2006002','B200201003','20110912','20111012','20111008');
-insert into Borrow values ('R2007001','B200201002','20110914','20111014',null);
-insert into Borrow values ('R2007001','B200201003','20110915','20111015','20111010');
-insert into Borrow values ('R2009002','B200201001','20110916','20111016','20111014');
-insert into Borrow values ('R2007002','B200201002','20110917','20111017','20111014');
-insert into Borrow values ('R2007002','B200201003','20110917','20111017','20111014');
-
--- 3ÊµÑé±íµÄ´´½¨ºÍÊı¾İ²Ù×İ
+-- 3å®éªŒè¡¨çš„åˆ›å»ºå’Œæ•°æ®æ“çºµ
 create database DB3;
---Ñ§Éú±í´´½¨
+-------------------------------------------------------------å­¦ç”Ÿè¡¨åˆ›å»º
 create table Student (
 	Sno char(7) primary key,
 	Sname char(10) not null,
-	Ssex char(2) check(Ssex='ÄĞ' or Ssex='Å®'),
+	Ssex char(2) check(Ssex='ç”·' or Ssex='å¥³'),
 	Sage smallint check(Sage>=15 and Sage<=45),
-	Sdept char(20) default '¼ÆËã»úÏµ'
+	Sdept char(20) default 'è®¡ç®—æœºç³»'
 );
---¿Î³ÌĞÅÏ¢±í´´½¨
+---------------------------------------------------------è¯¾ç¨‹ä¿¡æ¯è¡¨åˆ›å»º
 create table Course (
 	Cno char(10) primary key,
 	Cname char(20) not null,
 	Ccredit int check(Ccredit>0),
 	Semster int check(Semster>0)
 );
---Ñ§ÉúÑ¡¿Î±í´´½¨
+---------------------------------------------------------å­¦ç”Ÿé€‰è¯¾è¡¨åˆ›å»º
 create table SC (
 	Sno char(7) foreign key references Student(Sno),
 	Cno char(10) foreign key references Course(Cno),
@@ -112,50 +24,49 @@ create table SC (
 	primary key(Sno,Cno)
 );
 
---Ñ§Éú±íÊı¾İ²åÈë
-insert into Student values ('9512101','ÀîÓÂ','ÄĞ',19,'¼ÆËã»úÏµ');
-insert into Student values ('9512102','Áõ³¿','ÄĞ',20,'¼ÆËã»úÏµ');
-insert into Student values ('9512103','ÍõÃô','Å®',20,'¼ÆËã»úÏµ');
-insert into Student values ('9521101','ÕÅÁ¢','ÄĞ',22,'ĞÅÏ¢Ïµ');
-insert into Student values ('9521102','Îâ±ö','Å®',21,'ĞÅÏ¢Ïµ');
-insert into Student values ('9521103','ÕÅº£','ÄĞ',20,'ĞÅÏ¢Ïµ');
-insert into Student values ('9531101','Ç®Ğ¡Æ½','Å®',18,'ÊıÑ§Ïµ');
-insert into Student values ('9531102','Íõ´óÁ¦','ÄĞ',19,'ÊıÑ§Ïµ');
---¿Î³Ì±í²åÈë
-insert into Course values ('C01','¼ÆËã»úÎÄ»¯»ù´¡',3,1);
+----------------------------------------------------------å­¦ç”Ÿè¡¨æ•°æ®æ’å…¥
+insert into Student values ('9512101','æå‹‡','ç”·',19,'è®¡ç®—æœºç³»');
+insert into Student values ('9512102','åˆ˜æ™¨','ç”·',20,'è®¡ç®—æœºç³»');
+insert into Student values ('9512103','ç‹æ•','å¥³',20,'è®¡ç®—æœºç³»');
+insert into Student values ('9521101','å¼ ç«‹','ç”·',22,'ä¿¡æ¯ç³»');
+insert into Student values ('9521102','å´å®¾','å¥³',21,'ä¿¡æ¯ç³»');
+insert into Student values ('9521103','å¼ æµ·','ç”·',20,'ä¿¡æ¯ç³»');
+insert into Student values ('9531101','é’±å°å¹³','å¥³',18,'æ•°å­¦ç³»');
+insert into Student values ('9531102','ç‹å¤§åŠ›','ç”·',19,'æ•°å­¦ç³»');
+--è¯¾ç¨‹è¡¨æ’å…¥
+insert into Course values ('C01','è®¡ç®—æœºæ–‡åŒ–åŸºç¡€',3,1);
 insert into Course values ('C02','VB',2,3);
-insert into Course values ('C03','¼ÆËã»úÍøÂç',4,7);
-insert into Course values ('C04','Êı¾İ¿â»ù´¡',6,6);
-insert into Course values ('C05','¸ßµÈÊıÑ§',8,2);
-insert into Course values ('C06','Êı¾İ½á¹¹',5,4);
---Ñ¡¿Î±í²åÈë
-insert into SC values ('9512101','C01',90,'±ØĞŞ');
-insert into SC values ('9512101','C02',86,'Ñ¡ĞŞ');
-insert into SC values ('9512101','C06',62,'±ØĞŞ');
-insert into SC values ('9512102','C02',78,'Ñ¡ĞŞ');
-insert into SC values ('9512102','C04',66,'±ØĞŞ');
-insert into SC values ('9512102','C01',82,'Ñ¡ĞŞ');
-insert into SC values ('9512102','C05',92,'±ØĞŞ');
-insert into SC values ('9512102','C06',50,'±ØĞŞ');
-insert into SC values ('9512103','C02',68,'Ñ¡ĞŞ');
-insert into SC values ('9512103','C06',62,'±ØĞŞ');
-insert into SC values ('9531101','C01',80,'Ñ¡ĞŞ');
-insert into SC values ('9531101','C05',95,'±ØĞŞ');
-insert into SC values ('9531102','C05',85,'±ØĞŞ');
---Êı¾İ²Ù×İ
-update Student set Sdept='ĞÅÏ¢Ïµ' where Sname='ÍõÃô';
-update Course set Cname='Êı¾İ¿âÔ­ÀíÓëÓ¦ÓÃ' where Cname='Êı¾İ¿â»ù´¡';
+insert into Course values ('C03','è®¡ç®—æœºç½‘ç»œ',4,7);
+insert into Course values ('C04','æ•°æ®åº“åŸºç¡€',6,6);
+insert into Course values ('C05','é«˜ç­‰æ•°å­¦',8,2);
+insert into Course values ('C06','æ•°æ®ç»“æ„',5,4);
+--é€‰è¯¾è¡¨æ’å…¥
+insert into SC values ('9512101','C01',90,'å¿…ä¿®');
+insert into SC values ('9512101','C02',86,'é€‰ä¿®');
+insert into SC values ('9512101','C06',62,'å¿…ä¿®');
+insert into SC values ('9512102','C02',78,'é€‰ä¿®');
+insert into SC values ('9512102','C04',66,'å¿…ä¿®');
+insert into SC values ('9512102','C01',82,'é€‰ä¿®');
+insert into SC values ('9512102','C05',92,'å¿…ä¿®');
+insert into SC values ('9512102','C06',50,'å¿…ä¿®');
+insert into SC values ('9512103','C02',68,'é€‰ä¿®');
+insert into SC values ('9512103','C06',62,'å¿…ä¿®');
+insert into SC values ('9531101','C01',80,'é€‰ä¿®');
+insert into SC values ('9531101','C05',95,'å¿…ä¿®');
+insert into SC values ('9531102','C05',85,'å¿…ä¿®');
+------------------------------------------------------------æ•°æ®æ“çºµ
+update Student set Sdept='ä¿¡æ¯ç³»' where Sname='ç‹æ•';
+update Course set Cname='æ•°æ®åº“åŸç†ä¸åº”ç”¨' where Cname='æ•°æ®åº“åŸºç¡€';
 delete from SC where Grade<40;
 
---²éÑ¯
+----------------------------------------------------------------æŸ¥è¯¢
 
--- rj5
 -- 1
 select Reader.readerName,Borrow.borrowDate,Borrow.returnDate from Borrow,Reader where Reader.readerNO=Borrow.readerNO and bookNO='B200101002';
 -- 2
 select Borrow.bookNO,Book.bookName,Borrow.borrowDate,Borrow.returnDate 
 from Borrow,Book,Reader 
-where Borrow.bookNO=Book.bookNO and Borrow.readerNO=Reader.readerNO and Reader.readerName='ÕÅÏşÃ·';
+where Borrow.bookNO=Book.bookNO and Borrow.readerNO=Reader.readerNO and Reader.readerName='å¼ æ™“æ¢…';
 -- 3
 select Reader.readerNO,Reader.readerName,Book.bookName,Borrow.borrowDate,Borrow.returnDate 
 from Borrow,Book,Reader 
@@ -163,23 +74,23 @@ where Borrow.bookNO=Book.bookNO and Reader.readerNO=Borrow.readerNO and Borrow.r
 -- 4
 select Reader.readerNO,Reader.readerName,Book.bookName,Borrow.borrowDate,Borrow.returnDate 
 from Reader,Book,Borrow 
-where Reader.readerNO=Borrow.readerNO and Book.bookNO=Borrow.bookNO and Book.publishingName='Çå»ª´óÑ§³ö°æÉç';
+where Reader.readerNO=Borrow.readerNO and Book.bookNO=Borrow.bookNO and Book.publishingName='æ¸…åå¤§å­¦å‡ºç‰ˆç¤¾';
 -- 5
 select Borrow.readerNO,Reader.readerName,Book.bookNO,Book.bookName 
 from Borrow,Reader,Book 
 where Borrow.bookNO=Book.bookNO and Borrow.readerNO=Reader.readerNO and Borrow.borrowDate between '20100101' and '20111231';
 -- 6
-select 'Í¼Êé·ÖÀàºÅ'=Book.classNO,'×î¸ß¼Û¸ñ'=max(Book.price),'Æ½¾ù¼Û¸ñ'=avg(Book.price) 
+select 'å›¾ä¹¦åˆ†ç±»å·'=Book.classNO,'æœ€é«˜ä»·æ ¼'=max(Book.price),'å¹³å‡ä»·æ ¼'=avg(Book.price) 
 from Book 
 group by Book.classNO 
 order by max(Book.price) desc;
 -- 7
-select 'Èë¿âÊıÁ¿'=sum(Book.shopNum) 
+select 'å…¥åº“æ•°é‡'=sum(Book.shopNum) 
 from Book 
 group by Book.classNO 
 having Book.classNO<'009';
 -- 8
-select '¶ÁÕß±àºÅ'=t.rn,'¶ÁÕßĞÕÃû'=t.rm,'Ëù½èÍ¼Êé×Ü¼Û'=sum(t.bp)
+select 'è¯»è€…ç¼–å·'=t.rn,'è¯»è€…å§“å'=t.rm,'æ‰€å€Ÿå›¾ä¹¦æ€»ä»·'=sum(t.bp)
 from (
 select 'rn'=Reader.readerNO,'rm'=Reader.readerName,'bp'=Book.price 
 from Reader,Book,Borrow 
@@ -202,4 +113,58 @@ where Reader.readerNO=Borrow.readerNO and Book.bookNO=Borrow.bookNO and Reader.r
 select distinct Borrow.readerNO 
 from Borrow
 where Borrow.bookNO LIKE 'B2001*'
+);
+-- 11
+select Reader.readerNO,Reader.readerName,Borrow.borrowDate,Book.bookName 
+from Reader,Borrow,Book 
+where Reader.readerNO=Borrow.readerNO and Borrow.bookNO=Book.bookNO and Book.bookName in ('å¤§å­¦è‹±è¯­','å•†åŠ¡è‹±è¯­');
+-- 12
+select distinct Reader.readerNO,Reader.readerName
+from (
+	select 'rn'=Borrow.readerNO 
+	from Borrow,BookClass,Book 
+	where Borrow.bookNO=Book.bookNO and Book.classNO=BookClass.classNo and BookClass.className not in ('ç»æµç±»')
+) as t,Reader 
+where Reader.readerNO=t.rn;
+-- 13
+select Reader.readerNO,Reader.readerName,Reader.workUnit 
+from Reader,Borrow 
+where Borrow.bookNO in (
+	select Borrow.bookNO 
+	from Borrow,Reader
+	where Borrow.readerNO=Reader.readerNO and Reader.readerName='å¼ æ™“æ¢…'
+);
+-- 14
+select Reader.readerNO,Reader.readerName,Book.bookName,Borrow.borrowDate 
+from Reader,Borrow,Book 
+where Reader.readerNO=Borrow.readerNO and Book.bookNO=Borrow.bookNO and Book.classNO='002';
+-- 15
+select Borrow.readerNO 
+from Borrow 
+group by Borrow.readerNO 
+having count(Borrow.bookNO)>=3 
+order by Borrow.readerNO;
+-- 16
+select Reader.readerNO,Reader.readerName,SUBSTRING(Reader.identitycard,7,6) 
+from (
+	select top 1 'rn'=t1.readerNO 
+	from (
+		select 'readerNO'=Borrow.readerNO,'price'=Book.price from Borrow,Book where Borrow.bookNO=Book.bookNO
+	)as t1 
+	group by t1.readerNO 
+	order by sum(t1.price) desc
+) as t,Reader 
+where t.rn=Reader.readerNO;
+-- 17
+update Book set Book.price=Book.price+Book.price*0.1 
+where Book.classNO in (
+	select BookClass.classNo from BookClass 
+	where BookClass.className in ('ç»æµç±»')
+);
+-- 18
+select * from Borrow;
+update Borrow set Borrow.returnDate=getdate() 
+where Borrow.readerNO+Borrow.bookNO+convert(char(8),Borrow.borrowDate,112) in (
+	select Borrow.readerNO+Borrow.bookNO+convert(char(8),Borrow.borrowDate,112) from Borrow,Reader 
+	where Borrow.returnDate is null and Borrow.shouldDate < getdate() and Borrow.readerNO=Reader.readerNO and year(getdate()) - year(substring(Reader.identitycard,7,8)) between 25 and 35
 );
